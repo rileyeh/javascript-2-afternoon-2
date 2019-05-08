58108102554038ad1450f421dfbf61f1994e7357
Riley Hatch <Riley@Rileys-MacBook-Air-2.local>
2019-05-08 15:02:29 -0600

    thru number 4

--- practice.js
+++ practice.js
@@ -20,7 +20,7 @@ var arr = [10,20,30];
 //Code Here
 
 function first(arr) {
-  return arr[1]
+  return arr[0]
 }
 
 
@@ -38,6 +38,9 @@ var arr = [40,50,60];
 
 //Code Here
 
+function last(arr) {
+  return arr[2]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -53,6 +56,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
+function looper(family) {
+  for (let i = 0; i < family.length; i++) {
+    alert(family[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -68,6 +76,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
+function reversedLooper(letters) {
+  for (let i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i])
+  }
+}
+
 
 
 ////////// PROBLEM 5 //////////
@@ -82,6 +96,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+
+function evenFinder(nums) {
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+
+    }
+  }
+}
 
 
 
