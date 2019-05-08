03b6ca9cfec2fc464e0d7560c3238c7e828b3653
Riley Hatch <Riley@Rileys-MacBook-Air-2.local>
2019-05-08 15:14:47 -0600

    thru number 5

--- practice.js
+++ practice.js
@@ -98,12 +98,13 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Code Here
 
 function evenFinder(nums) {
-  
+  let evens = [];
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] % 2 === 0) {
-
+        evens.push(nums[i])
     }
   }
+  return evens
 }
 
 
